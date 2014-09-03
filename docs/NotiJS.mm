026eef0b3c0176182f43a8b95968a03597dd2482
Diego Malagón <diegomalagonh@gmail.com>
2014-09-03 08:29:58 -0500

    Diseño de entidad para mongoDB

---
+++ docs/NotiJS.mm
@@ -0,0 +1,133 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1409749845646" ID="ID_424616590" MODIFIED="1409750886819" TEXT="NotiJS">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Modelo para la aplicaci&#243;n NotiJS
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1409749856324" ID="ID_1418517006" MODIFIED="1409750866945" POSITION="right" TEXT="notice">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Entidad&#160;para notificaciones.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1409749860264" ID="ID_50618547" MODIFIED="1409750842384" TEXT="_id">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      id de la notificaci&#243;n.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1409749863871" ID="ID_1527340832" MODIFIED="1409750840126" TEXT="title">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      T&#237;tulo de la notificaci&#243;n.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1409749866408" ID="ID_689585186" MODIFIED="1409750936643" TEXT="body">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Cuerpo de la notificaci&#243;n.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1409750510542" ID="ID_1796700732" MODIFIED="1409750849729" TEXT="datetime">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Fecha y hora de creaci&#243;n de la notificaci&#243;n.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1409750498863" ID="ID_1983013077" MODIFIED="1409750833960" TEXT="img">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      url de imagen de la notificaci&#243;n. Puede ir vac&#237;o.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1409750671961" ID="ID_1855650879" MODIFIED="1409750819068" TEXT="url">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      url a donde debe dirigir la notificaci&#243;n. Puede ir vac&#237;o.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1409749871072" ID="ID_517693363" MODIFIED="1409750846584" TEXT="user_id">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      id del usuario al que pertence la notificaci&#243;n.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1409749876311" ID="ID_26089646" MODIFIED="1409750844786" TEXT="read">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Indica si la notificaci&#243;n ha sido le&#237;da o no por el usuario.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
