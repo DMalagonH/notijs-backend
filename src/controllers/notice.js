8ad53c6717141c293cad0638611222f7ee753ce7
Diego Malagón <diegomalagonh@gmail.com>
2014-10-02 08:47:54 -0500

    Consulta a MongoDB de las notificaciones de usuario ordenadas y con límite

--- src/controllers/notice.js
+++ src/controllers/notice.js
@@ -16,123 +16,28 @@ var Notice = require("../models/notice");
 app.get('/notices/:user_id/:num_items?', function(req, res){  
 
     var user_id = parseInt(req.params.user_id);
-    // set number of items to get. default is 10
-    var num_items = parseInt(req.params.num_items) || 10;
+    var num_items = parseInt(req.params.num_items) || null;
     
-	var notices = [
-		{
-			"_id":		"1654sdfssk6",
-			"title":	"Título",
-			"body":		"Contenido de la notificación",
-			"datetime":	"2014-09-03 12:51:00",
-			"img":		"/imgs/image.jpg",
-			"url":		"http://www.google.com",
-			"user_id":	user_id,
-			"read":		false
-		},
-		{
-			"_id":		"sd545433dw45",
-			"title":	"Título segunda notificación",
-			"body":		"Contenido de la notificación",
-			"datetime":	"2014-09-03 13:01:00",
-			"img":		null,
-			"url":		"/url/a/aplicacion",
-			"user_id":	user_id,
-			"read":		true
-		},
-		{
-			"_id":		"5d5a6fa657e4",
-			"title":	"Título tercera notificación",
-			"body":		"Contenido de la notificación",
-			"datetime":	"2014-09-03 13:10:00",
-			"img":		"http://url/de/la/imagen",
-			"url":		null,
-			"user_id":	user_id,
-			"read":		true
-		},
-		{
-			"_id":		"5d5a6fa657e4",
-			"title":	"Título tercera notificación",
-			"body":		"Contenido de la notificación",
-			"datetime":	"2014-09-03 13:10:00",
-			"img":		"http://url/de/la/imagen",
-			"url":		null,
-			"user_id":	user_id,
-			"read":		true
-		},
-		{
-			"_id":		"5d5a6fa657e4",
-			"title":	"Título tercera notificación",
-			"body":		"Contenido de la notificación",
-			"datetime":	"2014-09-03 13:10:00",
-			"img":		"http://url/de/la/imagen",
-			"url":		null,
-			"user_id":	user_id,
-			"read":		true
-		},
-		{
-			"_id":		"5d5a6fa657e4",
-			"title":	"Título tercera notificación",
-			"body":		"Contenido de la notificación",
-			"datetime":	"2014-09-03 13:10:00",
-			"img":		"http://url/de/la/imagen",
-			"url":		null,
-			"user_id":	user_id,
-			"read":		true
-		},
-		{
-			"_id":		"5d5a6fa657e4",
-			"title":	"Título tercera notificación",
-			"body":		"Contenido de la notificación",
-			"datetime":	"2014-09-03 13:10:00",
-			"img":		"http://url/de/la/imagen",
-			"url":		null,
-			"user_id":	user_id,
-			"read":		true
-		},
-		{
-			"_id":		"5d5a6fa657e4",
-			"title":	"Título tercera notificación",
-			"body":		"Contenido de la notificación",
-			"datetime":	"2014-09-03 13:10:00",
-			"img":		"http://url/de/la/imagen",
-			"url":		null,
-			"user_id":	user_id,
-			"read":		true
-		},
-		{
-			"_id":		"5d5a6fa657e4",
-			"title":	"Título tercera notificación",
-			"body":		"Contenido de la notificación",
-			"datetime":	"2014-09-03 13:10:00",
-			"img":		"http://url/de/la/imagen",
-			"url":		null,
-			"user_id":	user_id,
-			"read":		true
-		},
-		{
-			"_id":		"5d5a6fa657e4",
-			"title":	"Título tercera notificación",
-			"body":		"Contenido de la notificación",
-			"datetime":	"2014-09-03 13:10:00",
-			"img":		"http://url/de/la/imagen",
-			"url":		null,
-			"user_id":	user_id,
-			"read":		true
-		},
-	]
-
-	// Obtiene los x primeros elementos del array
-	notices = notices.splice(0, num_items);
-
-	res
-		.status(200)
-	    .set('Content-Type','application/json')
-	    .json({
-	      notices: notices
-	    });
+    // Obtener notificaciones del usuario ordenadas por fecha descendentemente y con el limite indicado
+    Notice.find({
+    	"user_id": user_id
+    })
+    .sort({
+		datetime: -1
+	})
+	.limit(num_items)
+	.exec(function(err, notices){
+    	// Response
+    	res
+			.status(200)
+			.set('Content-Type','application/json')
+			.json({
+				notices: notices
+			});
+    });
 });
 
+	
 /**
  * Función para crear una notificacion
  */
