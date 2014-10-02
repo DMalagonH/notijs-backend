6918b89e8a24369a485c6297e4fad5b9cde06d77
Diego Malagón <diegomalagonh@gmail.com>
2014-10-02 06:46:16 -0500

    Creación de modelo para notificaciones

--- src/models/notice.js
+++ src/models/notice.js
@@ -1,2 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var NoticeSchema = new Schema({
+	title: String,
+	body: String,
+	datetime: String,
+	img: String,
+	url: String,
+	user_id: Number,
+	read: Boolean
+});
+
+var model = mongoose.model("notice", NoticeSchema);
+
+module.exports = model;
