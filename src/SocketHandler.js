5a65b7552f87d62128739ca09e5f1311cc402451
Diego Malagón <diegomalagonh@gmail.com>
2014-10-09 09:05:57 -0500

    Manejador de sockets en archivo independiente

---
+++ src/SocketHandler.js
@@ -0,0 +1,63 @@
+var _ = require("lodash");
+var connections = [];
+
+module.exports = function(){
+
+	return {
+		handler: function(socket){
+		                
+			var addConnection = function(data){
+		    	var user_id = data.user_id;
+		    	var socket_id = socket.id;
+				var user_conn = findUserConnectionById(user_id);
+				
+		    	if(!user_conn){
+					user_conn = {
+						"user_id": 	user_id,
+						"sockets": 	[socket_id]  
+					} 
+					connections.push(user_conn);    		
+		    	}
+		    	else{
+		    		user_conn.sockets.push(socket_id);
+		    	}
+
+		    	socket.emit("serverSays", "Conectado!");
+		    };
+
+			var removeConnection = function(){
+				var socket_id = socket.id;
+		    	var user_conn = findUserConnectionBySocket(socket_id);
+
+		    	if(user_conn){
+		    		// Si hay mas de una conexión
+		    		if(user_conn.sockets.length > 1){
+
+		    			// Eliminar socket del arreglo de sockets del usuario
+		    			_.remove(user_conn.sockets, function(s){
+		    				return s === socket_id;
+		    			});
+		    		}
+		    		// Si hay solo una conexión
+		    		else if(user_conn.sockets.length === 1){
+		    			// Eliminar usuario del arreglo de conexiones
+		    			_.remove(connections, user_conn);
+		    		}
+		    	}	                	
+		    };
+
+		    function findUserConnectionById (user_id) {
+		    	return _.find(connections, {"user_id": user_id});
+		    }
+
+		    function findUserConnectionBySocket(socket_id){
+				return _.find(connections, {"sockets": [socket_id]});
+		    }
+
+		    socket.on("connection", addConnection);
+
+		    socket.on("disconnect", removeConnection);
+		},
+		connections: connections
+	};
+}
